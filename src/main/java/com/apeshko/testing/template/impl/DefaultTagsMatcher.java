e004379a4e27393143d93948b05950acc056e7cb
Artsiom <artsiom_peshko@epam.com>
2021-01-16 19:13:15 +0300

    add TagsMatcher impl

--- src/main/java/com/apeshko/testing/template/impl/DefaultTagsMatcher.java
+++ src/main/java/com/apeshko/testing/template/impl/DefaultTagsMatcher.java
@@ -2,11 +2,29 @@ package com.apeshko.testing.template.impl;
 
 import com.apeshko.testing.template.TagsMatcher;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DefaultTagsMatcher implements TagsMatcher {
+    private static final Pattern TAG_REGEX = Pattern.compile("#\\{(.+?)\\}", Pattern.DOTALL);
+
     @Override
     public List<String> getAllTags(String templateContent) {
-        return null;
+        if (templateContent == null) {
+            throw new IllegalArgumentException("template content should not be null");
+        }
+
+        final Set<String> tags = new HashSet<>();
+        final Matcher matcher = TAG_REGEX.matcher(templateContent);
+
+        while (matcher.find()) {
+            tags.add(matcher.group(1));
+        }
+
+        return new ArrayList<>(tags);
     }
 }
--- src/test/java/com/apeshko/testing/template/impl/DefaultTagsMatcherTest.java
+++ src/test/java/com/apeshko/testing/template/impl/DefaultTagsMatcherTest.java
@@ -2,6 +2,7 @@ package com.apeshko.testing.template.impl;
 
 import com.apeshko.testing.template.TagsMatcher;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,7 +19,19 @@ class DefaultTagsMatcherTest {
     @ParameterizedTest
     @MethodSource("provideTemplatesWithTags")
     void getAllTags_shouldReturnAllTagsInTemplate(String inputTemplate, List expectedResult) {
-        assertEquals(expectedResult, tagsMatcher.getAllTags(inputTemplate));
+        List<String> actualTags = tagsMatcher.getAllTags(inputTemplate);
+
+        assertTrue(expectedResult.containsAll(actualTags));
+        assertEquals(expectedResult.size(), actualTags.size());
+    }
+
+    @Test
+    void getAllTags_shouldThrowExceptionForWrongArgs() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            tagsMatcher.getAllTags(null);
+        });
+
+        assertEquals("template content should not be null", exception.getMessage());
     }
 
     private static Stream<Arguments> provideTemplatesWithTags() {
@@ -26,12 +39,6 @@ class DefaultTagsMatcherTest {
                 Arguments.of("hello #{name} #{surname}", Arrays.asList("name", "surname")),
                 Arguments.of("test", Arrays.asList()),
                 Arguments.of("#{test} word #{test}", Arrays.asList("test"))
-        );
-    }
-
-    private static Stream<Arguments> provideWrongArguments() {
-        return Stream.of(
-                Arguments.of(null, null)
         );
     }
 }
