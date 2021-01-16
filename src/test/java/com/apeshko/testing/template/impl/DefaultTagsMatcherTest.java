8fc4a9f78dc8c0eb1559a08e7d541d5c1a317168
Artsiom <artsiom_peshko@epam.com>
2021-01-16 20:35:55 +0300

    create custom extension (jUnit5) to output test execution information to file

--- src/test/java/com/apeshko/testing/template/impl/DefaultTagsMatcherTest.java
+++ src/test/java/com/apeshko/testing/template/impl/DefaultTagsMatcherTest.java
@@ -3,6 +3,7 @@ package com.apeshko.testing.template.impl;
 import com.apeshko.testing.template.TagsMatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,6 +14,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(TestExecutionLoggerToFile.class)
 class DefaultTagsMatcherTest {
     private final TagsMatcher tagsMatcher = new DefaultTagsMatcher();
 
